8f8126d762fb0b90d5f159da4f1b8e90f22224be
AdelaSeb <a.sebestova@outlook.com>
2019-05-21 15:37:38 -0700

    added new things

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  appId: '6b8f712f4579003a526985f1d4651864',
+  baseUrl: 'http://',
 };
 
 /*
